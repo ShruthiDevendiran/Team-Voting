bc18dfa5943c6db0cca636d3f2a64944364c856b
Shruthi <shruthidevendiran@gmail.com>
2022-07-30 18:26:24 +0530

    First change made

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAn5-VXk8g7JFkq6-B1LrQHpRDlYg86wBE",
+    authDomain: "voting-app-324b5.firebaseapp.com",
+    databaseURL: "https://voting-app-324b5-default-rtdb.firebaseio.com",
+    projectId: "voting-app-324b5",
+    storageBucket: "voting-app-324b5.appspot.com",
+    messagingSenderId: "671279162460",
+    appId: "1:671279162460:web:2e71ab92cce94788083299"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
